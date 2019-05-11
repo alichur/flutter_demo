745e6ae5071e95d0b5764188c7352aacf4713e5e
Ali Churcher <churcher.ali@gmail.com>
2019-05-11 16:19:25 +0200

    Separate sliver app bar

--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class OverviewScreen extends StatelessWidget {
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My app bar")),
-    );
+        appBar: AppBar(title: Text("My App Bar")),
+        body: CustomScrollView(slivers: [
+          SliverAppBar(
+            expandedHeight: 100,
+            title: Text(
+                'Sliver App Bar - hides on scroll'),
+            floating: true,
+            pinned: false,
+            snap: true,
+          ),
+          SliverFillRemaining(
+            child: Container(height: 600, child: Text('page contents')),
+          )
+        ]));
   }
 }
